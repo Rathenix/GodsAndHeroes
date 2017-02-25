b3beeb7467ccc73af40e29546d3b38bfb865ffa4
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-02-24 23:18:51 -0800

    Turn notification and Game Over screen

--- Assets/Scripts/GuiController.cs
+++ Assets/Scripts/GuiController.cs
@@ -9,14 +9,17 @@ public class GuiController : MonoBehaviour {
 
     public CellGrid CellGrid;
     public GameObject UnitsParent;
-    public Canvas Canvas;
 
     public GameObject InfoPanel;
     private GameObject _infoPanel;
+    public GameObject TurnPanel;
+    private GameObject _turnPanel;
 
     private void Start()
     {
-        
+        CellGrid.GameStarted += OnGameStarted;
+        CellGrid.TurnEnded += OnTurnEnded;
+        CellGrid.GameEnded += OnGameEnded;
         foreach (Transform unit in UnitsParent.transform)
         {
             unit.GetComponent<Unit>().UnitHighlighted += OnUnitHighlighted;
@@ -29,6 +32,7 @@ public class GuiController : MonoBehaviour {
     {
         var unit = sender as GhUnit;
         _infoPanel = Instantiate(InfoPanel);
+        var scroll = _infoPanel.transform.Find("InfoScroll");
 
         var stats = new StringBuilder();
         stats.Append(unit.HitPoints.ToString() + "/" + unit.MaxHitPoints.ToString()).AppendLine();
@@ -36,10 +40,10 @@ public class GuiController : MonoBehaviour {
         stats.Append(unit.DefenceFactor.ToString()).AppendLine();
         stats.Append(unit.MovementPoints.ToString());
 
-        _infoPanel.transform.Find("NameText").GetComponent<Text>().text = unit.DisplayName;
-        _infoPanel.transform.Find("StatsText").GetComponent<Text>().text = stats.ToString();
+        scroll.transform.Find("NameText").GetComponent<Text>().text = unit.DisplayName;
+        scroll.transform.Find("StatsText").GetComponent<Text>().text = stats.ToString();
 
-        _infoPanel.GetComponent<RectTransform>().SetParent(Canvas.GetComponent<RectTransform>(), false);
+        _infoPanel.transform.SetParent(transform, false);
     }
 
     private void OnUnitDehighlighted(object sender, EventArgs e)
@@ -50,6 +54,39 @@ public class GuiController : MonoBehaviour {
     private void OnUnitDestroyed(object sender, EventArgs e)
     {
         Destroy(_infoPanel);
+    }
+
+    private void OnGameStarted(object sender, EventArgs e)
+    {
+        StartCoroutine(ShowTurnPanel(false));
+    }
+
+    private void OnTurnEnded(object sender, EventArgs e)
+    {
+        StartCoroutine(ShowTurnPanel(false));
+    }
+
+    private void OnGameEnded(object sender, EventArgs e)
+    {
+        StartCoroutine(ShowTurnPanel(true));
+    }
+
+    IEnumerator ShowTurnPanel(bool gameOver)
+    {
+        if (_turnPanel != null) Destroy(_turnPanel);
+        _turnPanel = Instantiate(TurnPanel);
+        if (gameOver)
+        {
+            _turnPanel.GetComponentInChildren<Text>().text = "Game Over";
+        }
+        else
+        {
+            var playerName = CellGrid.CurrentPlayer.PlayerNumber == 0 ? "Player" : "Computer";
+            _turnPanel.GetComponentInChildren<Text>().text = playerName + "'s\nTurn";
+        }
+        _turnPanel.transform.SetParent(transform, false);
+        yield return new WaitForSeconds(1f);
+        if (!gameOver) Destroy(_turnPanel);
     }
 
     void Update () {
